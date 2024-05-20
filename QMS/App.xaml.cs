199786140bc0f8a18721170e9b3b03f88f06a000
linyang <lin772207039@gmail.com>
2024-05-20 14:57:36 +0800

    Remove submodule QMS and convert to regular folder

---
+++ QMS/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using QMS.View;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MyProject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainView>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+
+}
---
+++ QMS/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Mvvm;
+using QMS.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QMS.ViewModels
+{
+    internal class MainViewModel:NavigationViewModel
+    {
+
+    }
+}
