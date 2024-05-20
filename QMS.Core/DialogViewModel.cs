123b1ded80b4ceb2adb59bdefbe7899f6bb7f279
linyang <lin772207039@gmail.com>
2024-05-20 14:48:21 +0800

    变更结构

---
+++ QMS.Core/DialogViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Mvvm;
+using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QMS.Core
+{
+    public class DialogViewModel : BindableBase, IDialogAware
+    {
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        public event Action<IDialogResult> RequestClose;
+
+
+        /// <summary>
+        /// 是否可以关闭对话框
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public void OnDialogClosed()
+        {
+           
+        }
+
+        public  virtual void OnDialogOpened(IDialogParameters parameters)
+        {
+          
+        }
+    }
+}
---
+++ QMS.Core/NavigationViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QMS.Core
+{
+    /// <summary>
+    /// 导航基类
+    /// </summary>
+    public class NavigationViewModel : BindableBase, INavigationAware
+    {
+        /// <summary>
+        /// 是否重用导航对象
+        /// </summary>
+        /// <param name="navigationContext"></param>
+        /// <returns></returns>
+        public virtual bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+        /// <summary>
+        /// 导航切换是触发
+        /// </summary>
+        /// <param name="navigationContext"></param>
+
+        public virtual void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+     
+        }
+        /// <summary>
+        /// 到达被执行触发
+        /// </summary>
+        /// <param name="navigationContext"></param>
+
+        public virtual void OnNavigatedTo(NavigationContext navigationContext)
+        {
+          
+        }
+    }
+}
