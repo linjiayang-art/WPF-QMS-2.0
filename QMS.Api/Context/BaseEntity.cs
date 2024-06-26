b0addf3a21a4230f15a104ef8a5778eb88337bc2
linyang <lin772207039@gmail.com>
2024-06-26 16:05:02 +0800

    新增web api

---
+++ QMS.Api/Context/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace QMS.Api.Context
+{
+    public class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string CreatedBy { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+
+        public DateTime? UpdatedDate { get; set; }
+
+        public DateTime? DeletedDate { get; set; }
+    }
+}
